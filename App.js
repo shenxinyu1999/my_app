5b210a0210cc75b5ccbd668fc6a54e751eabfbb2
Valerie Liu <liur5@rose-hulman.edu>
2022-06-30 22:29:57 -0700

    frontend initial commit

--- App.js
+++ App.js
@@ -1,12 +1,51 @@
+import React from "react";
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, TextInput, Button } from 'react-native';
 
 export default function App() {
+  const [username, setUsername] = React.useState('');
+  const [password, setPassword] = React.useState('');
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <ScrollView>
+      <TextInput
+        style={{
+          height: 40,
+          borderColor: 'gray',
+          borderWidth: 1
+        }}
+        defaultValue="name"
+        onChangeText={setUsername}
+        value={username}
+      />
+      <TextInput
+        style={{
+          height: 40,
+          borderColor: 'gray',
+          borderWidth: 1
+        }}
+        defaultValue="password"
+        onChangeText={setPassword}
+        value={password}
+      />
       <StatusBar style="auto" />
-    </View>
+      <Button
+        title="Login"
+        onPress={() => {
+          fetch('http://localhost:3000/login', {
+            method: 'POST',
+            headers: {
+              Accept: 'application/json',
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+              name: username,
+              password: password
+            })
+          });
+        }}
+      />
+    </ScrollView>
   );
 }
 
